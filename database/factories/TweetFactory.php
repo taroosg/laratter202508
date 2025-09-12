b355c0e2574b997934b11f5d3ab4a33d5c638221
taroosg <root@taroosg.dev>
2025-09-12 19:38:13 +0900

    tweetとlikeのテストを作成

--- database/factories/TweetFactory.php
+++ database/factories/TweetFactory.php
@@ -3,21 +3,26 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Tweet;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Tweet>
  */
 class TweetFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            //
-        ];
-    }
+  protected $model = Tweet::class;
+
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'user_id' => User::factory(), // UserモデルのFactoryを使用してユーザを生成
+      'tweet' => $this->faker->text(200) // ダミーのテキストデータ
+    ];
+  }
 }
